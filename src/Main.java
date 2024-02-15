6c50e28364675265e54226fa6f2a7a5af7d7312b
Julian <julian.volk@mail.de>
2024-02-15 21:54:26 +0100

    lul

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Tolle Ausgabe aus der main()-Methode");
+        System.out.println("Tolle Ausgabe aus der mainmainmain()-Methode");
     }
 }
